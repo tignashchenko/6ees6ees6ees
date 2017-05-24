7b349a4f0c61e70c099b3f8b0dea30f9d50109b8
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-23 19:26:48 -0700

    Implement Bee class functionality

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor(food, eat) {
+    super(food, eat);
+    this.age = 5
+    this.color = 'yellow'
+    this.job = 'Keep on growing'
+  }
 };
