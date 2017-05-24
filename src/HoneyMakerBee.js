6fdeffeb62d8f534dbf536baf4e8736eea0bda79
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-23 19:31:57 -0700

    Implement HoneyMakerBee class functionality

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(color, food, eat) {
+    super(color, food, eat);
+    this.age = 10
+    this.job = 'make honey'
+    this.honeyPot = 0
+  }
+  makeHoney() {
+    this.honeyPot++;
+  }
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
