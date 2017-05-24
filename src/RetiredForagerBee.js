2ba3050ea7f074fa84da7758c59f477664e52d78
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-23 19:35:35 -0700

    Implement ForagerBee class functionality

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,12 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends HoneyMakerBee {
+  constructor(color, eat) {
+    super(color, eat);
+    this.age = 10
+    this.job = 'find pollen'
+    this.canFly = true
+    this.treasureChest = []
+  }
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
